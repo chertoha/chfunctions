991f101b7c071619c4a5b13ec4080771498de99d
Anton Chertok <a.chertok@akatech.com.ua>
2018-04-29 11:19:56 +0300

    Class Link

--- src/chFunc.php
+++ src/chFunc.php
@@ -1,16 +1,28 @@
 <?php
-
 namespace chertoha\functions;
+use mysqli;
 
-
-function connectionDB(){
-    $link = new mysqli('localhost', 'root', 'root', 'testsite');
-    $link->query("SET NAMES 'UTF8'") or die('Cannot set charset');
-    if (!empty($link->connect_error)){
-        die ('No connection to server, error: '.$link->connect_error);
-    }    
-    return $link;
+class Link{
+    private $host;
+    private $login;
+    private $password;
+    private $database;
+    
+    public function __construct($host, $login, $password, $database) {
+        $this->host = $host;
+        $this->login = $login;
+        $this->password = $password;
+        $this->database = $database;
+    }
+    
+    public function connectionDB() {
+        $link = new mysqli($this->host, $this->login, $this->password, $this->database);
+        $link->query("SET NAMES 'UTF8'") or die('Cannot set charset');
+        if (!empty($link->connect_error)) {
+            die('No connection to server, error: ' . $link->connect_error);
+        }
+        return $link;
+    }
+    
 }
-
-
 
